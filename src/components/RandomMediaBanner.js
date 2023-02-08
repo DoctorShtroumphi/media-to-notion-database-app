bc8b4a313cf6c3d655a1b6d8d21db49853c83124
Michael Masciotra <michael.masciotra.18@gmail.com>
2023-02-08 15:29:24 -0500

    Background is now properly blurred completely at all times.

--- src/components/RandomMediaBanner.js
+++ src/components/RandomMediaBanner.js
@@ -1,12 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
-import Card from 'react-bootstrap/Card';
-import * as Vibrant from 'node-vibrant';
 
 export default function RandomMediaBanner({ apiKey }) {
-  const todaysMediaType = new Date().getDate() % 2 === 1 ? 'movie' : 'tv';
+  const todaysMediaType = new Date().getDate() % 2 === 0 ? 'movie' : 'tv';
   const [media, setMedia] = useState('');
-  const [imgSrc, setImgSrc] = useState('');
 
   // Get trending media piece.
   useEffect(() => {
@@ -16,24 +13,7 @@ export default function RandomMediaBanner({ apiKey }) {
         setMedia(response.data.results[0]);
         // Image stuff
         var composedImgSrc = 'https://image.tmdb.org/t/p/original' + response.data.results[0].backdrop_path;
-        Vibrant.from(composedImgSrc).quality(1).getPalette().then((palette) => {
-          console.log(palette);
-          var gradColors = [
-            palette.DarkMuted.getRgb(),
-            palette.DarkVibrant.getRgb(),
-            palette.LightMuted.getRgb(),
-            palette.LightVibrant.getRgb(),
-            palette.Muted.getRgb(),
-            palette.Vibrant.getRgb()
-          ];
-
-          for (let index = 0; index < gradColors.length; index++) {
-            const element = gradColors[index];
-            console.log(element);
-          }
-          document.getElementsByTagName('body')[0].style.backgroundImage=`url(${composedImgSrc})`;
-        });
-        setImgSrc(composedImgSrc);
+        document.getElementsByTagName('body')[0].style.backgroundImage=`url(${composedImgSrc})`;
       })
       .catch(err => {
         console.error(err.message);
@@ -63,28 +43,9 @@ export default function RandomMediaBanner({ apiKey }) {
   }
 
   return (
-    <Card border='light' className="bg-dark text-white background-card">
-      <Card.Img src={imgSrc} alt="Card image" id='trending-media-banner'/>
-      <Card.ImgOverlay>
-        <Card.Title className='background-card-text'>{getTitle()}</Card.Title>
-        <Card.Text className='background-card-text'>{media.overview}</Card.Text>
-        <Card.Text className='background-card-text'>{getReleaseDate()}</Card.Text>
-      </Card.ImgOverlay>
-    </Card>
-  );
-
-  /*
-  return (
     <div>
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant='top' src={imgSrc} alt='IMAGE NOT FOUND' />
-        <Card.Body>
-          <Card.Title>{getTitle()}</Card.Title>
-          <Card.Text>
-            {getReleaseDate()}
-          </Card.Text>
-        </Card.Body>
-      </Card>
+      <h1>Trending today: {getTitle()} ({getReleaseDate()})</h1>
+      {media.overview}
     </div>
-  )*/
+  );
 }
